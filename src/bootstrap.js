482f3a508b5f118b35f223630e53cbd1b9c512b7
herly <herlysarmiento@gmail.com>
2020-02-12 18:34:59 -0700

    initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,8 @@
+// ES6 code goes here
+// import { salute, sume,} from './classChallenge'
+import classChallenge, { salute, suma } from './classChallenge';
+
+console.log(classChallenge());
+console.log(salute());
+console.log(suma(3, 2));
+
---
+++ src/classChallenge.js
@@ -0,0 +1,11 @@
+export default function() {
+    return '<div>classChallenge</div>'
+}
+
+export const salute = () =>  {
+        return "welcome"
+}
+
+export function suma(numOne, numTwo) {
+    return numOne + numTwo;
+}
